41b2230aadcb271701b6eb8613ce3da075c597a5
CharithBK <32832295+CharithBK@users.noreply.github.com>
2020-03-09 22:36:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testmysql/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.testmysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
